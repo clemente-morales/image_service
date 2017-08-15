7a40ffff02dccac8de1cd22dd3885188b18e390c
Clemente Morales Fernandez <clemente.morales@verizonwireless.com>
2017-08-15 14:51:26 -0400

    ImageService feature Add database scheme for photo gallery application

--- config/db.sql
+++ config/db.sql
@@ -5,18 +5,43 @@ create database photo_gallery;
 grant all privileges on photo_gallery.* to 'chicharron'@'localhost' identified by "ch1ch4rr0n";
 
 # Create table to save categories
+create table category (
+    id        tinyint(2),
+    name      varchar(255) not null,
+    description TEXT null,
+	created_at TIMESTAMP not null,
+	updated_at TIMESTAMP not null,
+	enabled tinyint(1),
+	PRIMARY KEY (id)
+)ENGINE=InnoDB;
 
 # Create table to save photographer
+create table photographer (
+    id        SMALLINT AUTO_INCREMENT,
+    name      varchar(255) not null,
+    email	varchar(255) not null,
+    phone      varchar(50) not null,
+	created_at TIMESTAMP not null,
+	updated_at TIMESTAMP not null,
+	enabled tinyint(1),
+	PRIMARY KEY (id)
+)ENGINE=InnoDB;
 
 # Create table to save images
 create table photo (
-    id        tinyint(3)  not null default '0',
-    category_id      tinyint(2) not null,
-    photographer_id tinyint(3) not null,
-    path           varchar(256) not null,
-    name      varchar(50) not null,
-    description      varchar(50) not null default ''
-);
+    id        INT NOT NULL AUTO_INCREMENT,
+    category_id      tinyint not null,
+    photographer_id SMALLINT not null,
+	name      varchar(255) not null,
+    image_path	varchar(255) not null,
+    description	TEXT null,
+	created_at TIMESTAMP not null,
+	updated_at TIMESTAMP not null,
+	enabled tinyint(1),
+	CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_photographer FOREIGN KEY (photographer_id) REFERENCES photographer(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	PRIMARY KEY (id)
+)ENGINE=InnoDB;
 
 
 
