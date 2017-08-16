3f0a2037c2bdc1fdd771fc7a8af8a7243b6ff767
Clemente Morales Fernandez <morales.fernandez.clemente@gmail.com>
2017-08-16 08:55:25 -0400

    ImageService feature Assign default values for table metadata columns

--- config/db.sql
+++ config/db.sql
@@ -9,9 +9,9 @@ create table category (
     id        tinyint(2),
     name      varchar(255) not null,
     description TEXT null,
-	created_at TIMESTAMP not null,
-	updated_at TIMESTAMP not null,
-	enabled tinyint(1),
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	enabled tinyint(1) DEFAULT 1,
 	PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
@@ -21,9 +21,9 @@ create table photographer (
     name      varchar(255) not null,
     email	varchar(255) not null,
     phone      varchar(50) not null,
-	created_at TIMESTAMP not null,
-	updated_at TIMESTAMP not null,
-	enabled tinyint(1),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        enabled tinyint(1) DEFAULT 1,
 	PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
@@ -35,9 +35,9 @@ create table photo (
 	name      varchar(255) not null,
     image_path	varchar(255) not null,
     description	TEXT null,
-	created_at TIMESTAMP not null,
-	updated_at TIMESTAMP not null,
-	enabled tinyint(1),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        enabled tinyint(1) DEFAULT 1,
 	CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES category(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT fk_photographer FOREIGN KEY (photographer_id) REFERENCES photographer(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY (id)
